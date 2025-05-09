4237e68b398cffd4ddf38d93f8595c23ec8dfef9
Mirzaadr <mirzaadr34@yahoo.com>
2025-05-09 23:31:20 +0700

    remove route

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,7 @@
-import { Route, Routes } from "react-router-dom";
-import BlogPage from "./pages/blogs";
 import Home from "./pages/Home";
-import ToolsPage from "./pages/tools";
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/blogs" element={<BlogPage />} />
-      <Route path="/tools" element={<ToolsPage />} />
-      <Route path="/" element={<Home />} />
-    </Routes>
-  );
+  return <Home />;
 }
 
 export default App;
