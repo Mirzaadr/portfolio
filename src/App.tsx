d346472082dea180597eefe786f622070363c655
Mirzaadr <mirzaadr34@yahoo.com>
2025-07-25 00:58:22 +0700

    try to use routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import BlogPage from "./pages/blogs";
 import Home from "./pages/Home";
+import ToolsPage from "./pages/tools";
 
 function App() {
-  return <Home />;
+  return (
+    <Routes>
+      <Route path="/blogs" element={<BlogPage />} />
+      <Route path="/tools" element={<ToolsPage />} />
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
 }
 
 export default App;
